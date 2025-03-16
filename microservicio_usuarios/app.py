4b5be7243b7594a01a5702d479f57ed1f917cb31
afDonoso <af.donoso@uniandes.edu.co>
2025-03-15 20:01:39 -0500

    Implementation of the hash validation alert

--- microservicio_usuarios/app.py
+++ microservicio_usuarios/app.py
@@ -1,17 +1,23 @@
-from flask_restful import Resource, Api
 from flask import Flask, request
+from flask_jwt_extended import get_jwt, JWTManager
+from flask_restful import Resource, Api
 
 from modelos import db, Usuario, UsuarioSchema, RolesEnum
-from utils import allowed_roles
+from utils import allowed_roles, generate_role_hash
 
 app = Flask(__name__)
 app.config["SQLALCHEMY_DATABASE_URI"] = (
     "postgresql://postgres:arquitectura@arquitecturas-agiles-software.ceebxtes3heo.us-east-1.rds.amazonaws.com:5432/usuarios"
 )
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+app.config["JWT_HEADER_NAME"] = "Authorization"
+app.config["JWT_HEADER_TYPE"] = "Bearer"
+app.config["JWT_SECRET_KEY"] = "super-secret"
 
 app_context = app.app_context()
 app_context.push()
+
+jwt = JWTManager(app)
 
 db.init_app(app)
 db.create_all()
@@ -19,26 +25,59 @@ db.create_all()
 api = Api(app)
 
 usuario_schema = UsuarioSchema()
+secret_key = b"clave_secreta"
 
 
 class VistaUsuarios(Resource):
-
     @allowed_roles([RolesEnum.LOGISTIC_DIRECTOR])
     def get(self):
         return 456
 
     @allowed_roles([RolesEnum.LOGISTIC_DIRECTOR])
     def post(self):
+        claims = get_jwt()
+        user_id: str = str(claims["sub"])
+        role_hash = generate_role_hash(request.json["role"], request.json["role"], user_id)
+
         nuevo_usuario = Usuario(
             nombre=request.json["nombre"],
             usuario=request.json["usuario"],
             contrasena=request.json["contrasena"],
-            type=request.json["type"],
+            role=request.json["role"],
+            prev_role=request.json["role"],
+            role_assigned_by=user_id,
+            role_hash=role_hash
         )
 
         db.session.add(nuevo_usuario)
         db.session.commit()
         return usuario_schema.dump(nuevo_usuario)
+
+class VistaUsuario(Resource):
+    @allowed_roles([RolesEnum.LOGISTIC_DIRECTOR])
+    def put(self, user_id):
+        usuario: Usuario = Usuario.query.get_or_404(user_id)
+
+        if "nombre" in request.json:
+            usuario.nombre = request.json["nombre"]
+
+        if "usuario" in request.json:
+            usuario.usuario = request.json["usuario"]
+
+        if "contrasena" in request.json:
+            usuario.contrasena = request.json["contrasena"]
+
+        if "role" in request.json:
+            claims = get_jwt()
+            responsible_user_id: str = str(claims["sub"])
+            usuario.prev_role = usuario.role
+            usuario.role = request.json["role"]
+            usuario.role_assigned_by = responsible_user_id
+            usuario.role_hash = generate_role_hash(usuario.prev_role.value, usuario.role, responsible_user_id)
+
+        db.session.commit()
+        return usuario_schema.dump(usuario)
+
     
 
 class VistaValidarUsuario(Resource):
@@ -53,5 +92,6 @@ class VistaValidarUsuario(Resource):
         return usuario_schema.dump(usuario)
 
 
-api.add_resource(VistaUsuarios, "/usuario")
 api.add_resource(VistaValidarUsuario, "/usuario/validar")
+api.add_resource(VistaUsuario, "/usuario/<int:user_id>")
+api.add_resource(VistaUsuarios, "/usuario")
--- microservicio_usuarios/utils.py
+++ microservicio_usuarios/utils.py
@@ -1,7 +1,12 @@
-from functools import wraps
 import hashlib
+import hmac
+from functools import wraps
+
 from flask_jwt_extended import verify_jwt_in_request, get_jwt
+
 from modelos.modelos import RolesEnum
+
+secret_key = b"clave_secreta"
 
 # Función para cifrar la clave de un usuario
 def cifrar(clave):
@@ -12,7 +17,7 @@ def allowed_roles(roles: list[RolesEnum]):
     def wrapper(fn):
         @wraps(fn)
         def decorator(*args, **kwargs):
-            verify_jwt_in_request()
+            verify_jwt_in_request(locations='headers')
             claims = get_jwt()
             if any(claims["role"] == role.value for role in roles):
                 return fn(*args, **kwargs)
@@ -22,3 +27,8 @@ def allowed_roles(roles: list[RolesEnum]):
         return decorator
 
     return wrapper
+
+
+def generate_role_hash(prev_role: str, new_role: str, user_id: str):
+    role_change: str = prev_role + "-" + new_role
+    return hmac.new(secret_key + user_id.encode(), role_change.encode(), hashlib.sha256).hexdigest()
